808d373f88468854ffa100654540577e4eea0b87
Steve Birtles <sbirtles@farnborough.ac.uk>
2020-12-04 20:09:55 +0000

    Day 4 complete

--- src/day4.cpp
+++ src/day4.cpp
@@ -4,23 +4,25 @@
 #include <tuple>
 #include <vector>
 
-int main() {
-  std::array<std::string, 8> keys = {"byr", "iyr", "eyr", "hgt",
-                                     "hcl", "ecl", "pid", "cid"};
+using stringPair = std::pair<std::string, std::string>;
+using pairVec = std::vector<stringPair>;
 
-  std::vector<std::vector<std::tuple<std::string, std::string>>*> passports;
+int main() {
+  std::string keys[]{"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"};
+
+  std::vector<pairVec*> passports;
 
   std::string line;
   std::ifstream myfile("./dat/day4.txt");
 
   if (myfile.is_open()) {
-    auto* codes = new std::vector<std::tuple<std::string, std::string>>;
+    auto* pairs = new pairVec;
 
     while (std::getline(myfile, line)) {
       try {
         if (line == "") {
-          passports.push_back(codes);
-          codes = new std::vector<std::tuple<std::string, std::string>>;
+          passports.push_back(pairs);
+          pairs = new pairVec;
           std::cout << std::endl;
         } else {
           int start_key = 0;
@@ -29,7 +31,7 @@ int main() {
           for (int i = 0; i < line.length(); i++) {
             if (line[i] == ' ' || i == line.length() - 1) {
               if (start_value != -1) {
-                codes->push_back(std::make_tuple(
+                pairs->push_back(std::make_pair(
                     line.substr(start_key, 3),
                     line.substr(
                         start_value,
@@ -51,88 +53,95 @@ int main() {
         std::cout << "An error occured: " << e << std::endl;
       }
     }
-    passports.push_back(codes);
+    passports.push_back(pairs);
     std::cout << std::endl;
 
     myfile.close();
 
-    int count = 0;
+    int count1 = 0, count2 = 0;
 
-    for (int i = 0; i < passports.size(); i++) {
-      bool checks[] = {false, false, false, false, false, false, false, false};
+    for (auto pairs : passports) {
+      bool checks1[] = {false, false, false, false, false, false, false};
+      bool checks2[] = {false, false, false, false, false, false, false};
 
-      auto& codes = *passports[i];
+      for (auto pair : *pairs) {
+        for (int k = 0; k < 7; k++) {
+          std::string key = pair.first;
 
-      for (int j = 0; j < codes.size(); j++) {
-        for (int k = 0; k < 8; k++) {
-          std::string code = std::get<0>(codes[j]);
-          if (keys[k] == code) {
-            std::string value = std::get<1>(codes[j]);
+          if (keys[k] == key) {
+            checks1[k] = true;
+
+            std::string value = pair.second;
 
             try {
-              if (code == "byr") {
+              if (key == "byr") {
                 int year = std::stoi(value);
-                if (year >= 1920 && year <= 2002) checks[k] = true;
+                if (year >= 1920 && year <= 2002) checks2[k] = true;
               }
 
-              if (code == "iyr") {
+              if (key == "iyr") {
                 int year = std::stoi(value);
-                if (year >= 2010 && year <= 2020) checks[k] = true;
+                if (year >= 2010 && year <= 2020) checks2[k] = true;
               }
 
-              if (code == "eyr") {
+              if (key == "eyr") {
                 int year = std::stoi(value);
-                if (year >= 2020 && year <= 2030) checks[k] = true;
+                if (year >= 2020 && year <= 2030) checks2[k] = true;
               }
 
-              if (code == "hgt") {
-                int height = std::stoi(value.substr(0, value.length() - 2));
+              if (key == "hgt") {
                 std::string units = value.substr(value.length() - 2, 2);
-                if (units == "cm" || units == "in") checks[k] = true;
-              }
-
-              if (code == "hcl") {
-              }
-
-              if (code == "ecl") {
-                if (value == "amb" || value == "blu" || value == "brn" ||
-                    value == "gry" || value == "grn" || value == "hzl" ||
-                    value == "oth")
-                  checks[k] = true;
-              }
-
-              if (code == "pid") {
-                if (value.length() == 9) {
-                  std::stoi(value);
+                if (units == "cm" || units == "in") {
+                  int height = std::stoi(value.substr(0, value.length() - 2));
+                  if (height >= 150 && height <= 193 && units == "cm" ||
+                      height >= 59 && height <= 76 && units == "in") {
+                    checks2[k] = true;
+                  }
                 }
               }
 
-              if (code == "cid") checks[k] = true;
+              if (key == "hcl") {
+                if (value.length() == 7 && value[0] == '#') {
+                  int hex = std::stoi(value.substr(1, 6), nullptr, 16);
+                  checks2[k] = true;
+                }
+              }
+
+              if (key == "ecl") {
+                if (value == "amb" || value == "blu" || value == "brn" ||
+                    value == "gry" || value == "grn" || value == "hzl" ||
+                    value == "oth")
+                  checks2[k] = true;
+              }
+
+              if (key == "pid") {
+                if (value.length() == 9) {
+                  std::stoi(value);
+                  checks2[k] = true;
+                }
+              }
 
             } catch (int e) {
-              std::cout << "An error occured: " << e << std::endl;
+              std::cerr << "An error occured: " << e << std::endl;
             }
+
+            break;
           }
         }
       }
 
-      // std::cout << std::endl;
-
-      bool valid = true;
-      for (int k = 0; k < 8; k++) {
-        if (keys[k] == "cid") continue;
-        if (!checks[k]) {
-          //  std::cout << "Missing " << keys[k] << std::endl;
-          valid = false;
-        }
+      bool valid1 = true, valid2 = true;
+      for (int k = 0; k < 7; k++) {
+        if (!checks1[k]) valid1 = false;
+        if (!checks2[k]) valid2 = false;
       }
 
-      // std::cout << std::endl;
-
-      if (valid) count++;
+      if (valid1) count1++;
+      if (valid2) count2++;
     }
 
-    std::cout << "Part 1 " << count << " / " << passports.size() << std::endl;
+    std::cout << "Part 1 " << count1 << " / " << passports.size() << std::endl;
+    std::cout << "Part 2 " << count2 << " / " << passports.size() << std::endl;
 
   } else {
     std::cout << "Unable to open file";
